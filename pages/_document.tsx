2f29157848d759931fa7bdf5b3f9799ea07b972c
turtle601 <turtle601@naver.com>
2022-08-19 16:13:56 +0900

    fix : eslint(airbnb) + prettier 버그 수정 [#5]

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+ Html, Head, Main, NextScript,
+} from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
