7d6855d5026c977ada1c7e63f7bb947c740786e3
turtle601 <turtle601@naver.com>
2022-08-22 02:18:35 +0900

    Fix : Next+Recoil key 중복 선언 에러 해결 [#18] uuid 라이브러리 설치 및 적용

--- atom/atom.theme.ts
+++ atom/atom.theme.ts
@@ -1,13 +1,14 @@
 import { atom, selector } from 'recoil';
+import { v1 } from 'uuid';
 import { lightTheme, darkTheme } from '@/stylesheets/theme';
 
 export const themeState = atom({
-  key: 'themeState',
+  key: `themeState${v1()}`,
   default: 'light',
 });
 
 export const themeSelector = selector({
-  key: 'themeSelector',
+  key: `themeSelector${v1()}`,
   get: ({ get }) => {
     const theme = get(themeState);
     return theme === 'light' ? lightTheme : darkTheme;
