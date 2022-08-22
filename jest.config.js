1da622b583b8bf45a1edaca4d27866ff43421c97
turtle601 <turtle601@naver.com>
2022-08-22 23:07:33 +0900

    Chores: config 폴더 절대경로 설정 [#21]

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
     '^@/public/(.*)$': '<rootDir>/public/$1',
     '^@/stylesheets/(.*)$': '<rootDir>/lib/stylesheets/$1',
     '^@/hooks/(.*)$': '<rootDir>/lib/hooks/$1',
+    '^@/config/(.*)$': '<rootDir>/lib/config/$1',
     '^@/atom/(.*)$': '<rootDir>/atom/$1',
     'uuid': require.resolve('uuid')
   },
