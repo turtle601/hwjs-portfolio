eac666679bde4efb8546071969545eebb20b67ed
turtle601 <turtle601@naver.com>
2022-08-23 12:59:47 +0900

    Fix : 환경 변수 값 수정 [#21] next에서 .env 파일은 브라우저에서 적용 X -> NEXT_PUBLIC 접두사 붙이면 가능

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,2 +1,2 @@
-export const DATABASE_ID = process.env.NOTION_DATABASE_ID;
-export const TOKEN = process.env.NOTION_TOKEN;
+export const DATABASE_ID = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
+export const TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN;
