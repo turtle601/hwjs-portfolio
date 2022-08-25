eec5331dffd9c730166a8d3dcd5839577e920b2d
turtle601 <turtle601@naver.com>
2022-08-25 15:23:32 +0900

    Styles: footer, header, Layout 컴포넌트 html 태그 스타일 변경 [#23]

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,25 +3,23 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import oc from 'open-color';
 
-import { shadow, sizes, media } from '@/stylesheets/utils';
+import { shadow, sizes } from '@/stylesheets/utils';
 import { ReactProps } from '@/types/common.types';
 
 import Button from '@/components/common/Button';
 import DarkModeButton from '@/components/home/DarkModeButton';
 
 const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: column;
   position: sticky;
   top: 0px;
+  
   width: 100%;
-  ${shadow(1)}
-`;
-
-const WhiteBackground = styled.div`
+  height: auto;
+  
   display: flex;
   justify-content: center;
-  height: auto;
+  
+  ${shadow(1)}
 `;
 
 const HeaderContents = styled.header`
@@ -33,8 +31,8 @@ const HeaderContents = styled.header`
 
   padding-left: 1rem;
   padding-right: 1rem;
- 
-  @media ${media.wide} {
+
+  @media ${sizes.wide} {
     width: 100%;
   }
 `;
@@ -73,18 +71,16 @@ const BtnNavbar = styled.nav`
 const Header: React.FC<ReactProps> = () => {
   return (
     <HeaderContainer>
-      <WhiteBackground>
-        <HeaderContents>
-          <Logo>준승 potofolio</Logo>
-          <Spacer />
-          <BtnNavbar>
-            <Button data-testid="homeBtn" href="/" variant="link" colorScheme={oc.cyan[6]}>홈</Button>
-            <Button data-testid="projectBtn" href="/project" variant="link" colorScheme={oc.cyan[6]}>프로젝트</Button>
-            <Button data-testid="connectBtn" href="/connect" variant="link" colorScheme={oc.cyan[6]}>연결하기</Button>
-            <DarkModeButton />
-          </BtnNavbar>
-        </HeaderContents>
-      </WhiteBackground>
+      <HeaderContents>
+        <Logo>준승 potofolio</Logo>
+        <Spacer />
+        <BtnNavbar>
+          <Button data-testid="homeBtn" href="/" variant="link" colorScheme={oc.cyan[6]}>홈</Button>
+          <Button data-testid="projectBtn" href="/project" variant="link" colorScheme={oc.cyan[6]}>프로젝트</Button>
+          <Button data-testid="connectBtn" href="/connect" variant="link" colorScheme={oc.cyan[6]}>연결하기</Button>
+          <DarkModeButton />
+        </BtnNavbar>
+      </HeaderContents>
       <GradientBorder />
     </HeaderContainer>
   );
--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
+import styled from 'styled-components';
 import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 
 import { ReactProps } from '@/types/common.types';
 
+const StyledLayout = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
 const Layout: React.FC<ReactProps> = ({ children }) => {
   return (
-    <>
+    <StyledLayout>
       <Header />
       {children}
       <Footer />
-    </>
-);
+    </StyledLayout>
+  );
 };
 
 export default Layout;
