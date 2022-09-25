a7d1e9320cda4a837a3466728d222ebdb75552cb
turtle601 <turtle601@naver.com>
2022-09-25 13:51:33 +0900

    Feat : fadein CSS 애니메이션 스타일 변수 생성 [#32]

---
+++ lib/stylesheets/animations.ts
@@ -0,0 +1,11 @@
+import { css, keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from { opacity: 0 }
+  to { opacity: 1 }
+`;
+
+export const fadeInAnimated = css`
+  opacity: 0;
+  animation: ${fadeIn} 2s both; 
+`;
