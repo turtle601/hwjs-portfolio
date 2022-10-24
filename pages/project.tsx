951de0e09ddcdcb4bd7ac130d4ee87cbf65bf736
turtle601 <turtle601@naver.com>
2022-10-24 22:21:33 +0900

    Styles: 기본 폰트 배민 주아 폰트로 변경 [#48]

--- pages/project.tsx
+++ pages/project.tsx
@@ -25,6 +25,7 @@ const Project: NextPage = () => {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>준승&apos;s potofolio</title>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rajdhani:700" />
+          <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet" />
         </Helmet>
       </HelmetProvider>
       <Layout>
